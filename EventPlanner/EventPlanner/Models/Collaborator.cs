20d78fd80cf5bd0df73da6ebf3da356d1317b583
jelenaseka <44838894+jelenaseka@users.noreply.github.com>
2021-05-31 19:57:42 +0200

    creating CollaboratorViewModel

---
+++ EventPlanner/EventPlanner/Models/Collaborator.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace EventPlanner.Models
+{
+    public enum CollaboratorType { FLOWER_SHOP, RESTAURANT, BALLOONS, DRINK_STORE }
+    public class Collaborator : INotifyPropertyChanged
+    {
+        private string _Name;
+        private string _Address;
+        private CollaboratorType _Type;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public String Name {
+            get {
+                return _Name;
+            }
+            set {
+                _Name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+
+        public String Address
+        {
+            get
+            {
+                return _Address;
+            }
+            set
+            {
+                _Address = value;
+                OnPropertyChanged("Address");
+            }
+        }
+
+        public CollaboratorType Type {
+            get
+            {
+                return _Type;
+            }
+            set
+            {
+                _Type = value;
+                OnPropertyChanged("Type");
+            }
+        }
+
+        public Collaborator(string name, CollaboratorType type, string address)
+        {
+            Name = name;
+            Type = type;
+            Address = address;
+        }
+
+        public Collaborator() { }
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+
+            if(handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
---
+++ EventPlanner/EventPlanner/ViewModels/CollaboratorViewModel.cs
@@ -0,0 +1,71 @@
+﻿using EventPlanner.Commands;
+using EventPlanner.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Text;
+using System.Windows.Input;
+
+namespace EventPlanner.ViewModels
+{
+    internal class CollaboratorViewModel : INotifyPropertyChanged
+    {
+        public CollaboratorViewModel()
+        {
+            List<Collaborator> collaborators = new List<Collaborator>();
+            collaborators.Add(new Collaborator("Zikina klopa", CollaboratorType.RESTAURANT, "Adresa 1"));
+            collaborators.Add(new Collaborator("Marinini baloni", CollaboratorType.BALLOONS, "Adresa 2"));
+            collaborators.Add(new Collaborator("Pice kod Mice", CollaboratorType.DRINK_STORE, "Adresa 3"));
+            collaborators.Add(new Collaborator("Pice kod Mice", CollaboratorType.DRINK_STORE, "Adresa 3"));
+            _Collaborators = new ObservableCollection<Collaborator>(collaborators);
+
+            UpdateListCommand = new CollaboratorListUpdateCommand(this);
+        }
+        public bool CanUpdate
+        {
+            get
+            {
+                return true;
+            }
+        }
+
+        private ObservableCollection<Collaborator> _Collaborators;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ObservableCollection<Collaborator> Collaborators
+        {
+            get
+            {
+                return _Collaborators;
+            }
+            set
+            {
+                _Collaborators = value;
+                OnPropertyChanged("_Collaborators");
+            }
+        }
+
+        public ICommand UpdateListCommand
+        {
+            get;
+            private set;
+        }
+
+        public void SaveChanges()
+        {
+            _Collaborators.RemoveAt(0); //proba
+        }
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
