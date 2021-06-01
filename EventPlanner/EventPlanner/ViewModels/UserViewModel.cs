0e5acd9d77076b5e26e43abecae6f5d0c8b95207
jelenaseka <44838894+jelenaseka@users.noreply.github.com>
2021-06-01 18:43:24 +0200

    Added ProfilePage and UserViewModel

---
+++ EventPlanner/EventPlanner/ViewModels/UserViewModel.cs
@@ -0,0 +1,37 @@
+﻿using EventPlanner.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventPlanner.ViewModels
+{
+    class UserViewModel : ObservableObject
+    {
+        private User user;
+        public bool isOrganizer;
+
+        public UserViewModel()
+        {
+            InitData();
+        }
+
+        private void InitData()
+        {
+            //user = new User("micko", "micko123", "Mica", "Lakic");
+            //isOrganizer = false;
+
+            user = new Organizer("micko", "micko123", "Mica", "Lakic", 3);
+            isOrganizer = true;
+        }
+        public User User
+        {
+            get => user;
+        }
+
+        public bool IsOrganizer
+        {
+            get => isOrganizer;
+            set { isOrganizer = value; RaisePropertyChngedEvent("IsOrganizer"); }
+        }
+    }
+}
