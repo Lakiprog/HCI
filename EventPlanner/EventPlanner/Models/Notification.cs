685e6b0d80e3d04076d116da448b40728ff14a8e
jelenaseka <44838894+jelenaseka@users.noreply.github.com>
2021-06-03 19:40:14 +0200

    Added delete notification command

--- EventPlanner/EventPlanner/Models/Notification.cs
+++ EventPlanner/EventPlanner/Models/Notification.cs
@@ -8,10 +8,16 @@ namespace EventPlanner.Models
     public enum NotificationType { MESSAGE_SENT, EVENT_REQUEST, EVENT_CHANGED }
     public class Notification : ObservableObject
     {
+        private long _Id;
         private User _User;
         private NotificationElement _NotificationElement;
         private NotificationType _Type;
 
+        public long Id
+        {
+            get => _Id;
+            set { _Id = value; RaisePropertyChngedEvent("Id"); }
+        }
         public User User
         {
             get => _User;
@@ -27,8 +33,9 @@ namespace EventPlanner.Models
             get => _Type;
             set { _Type = value; RaisePropertyChngedEvent("Type"); }
         }
-        public Notification(User user, NotificationElement notificationelement, NotificationType type)
+        public Notification(long id, User user, NotificationElement notificationelement, NotificationType type)
         {
+            Id = id;
             User = user;
             NotificationElement = notificationelement;
             Type = type;
--- EventPlanner/EventPlanner/ViewModels/NotificationsViewModel.cs
+++ EventPlanner/EventPlanner/ViewModels/NotificationsViewModel.cs
@@ -1,40 +1,64 @@
-﻿using EventPlanner.Models;
+﻿using EventPlanner.Commands;
+using EventPlanner.Models;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Text;
+using System.Windows.Input;
 
 namespace EventPlanner.ViewModels
 {
     class NotificationsViewModel : ObservableObject
     {
-        private List<NotificationViewModel> _NotificationViewModels;
+        private ObservableCollection<NotificationViewModel> _NotificationViewModels;
 
         public NotificationsViewModel()
         {
             InitData();
+            InitCommands();
         }
 
         private void InitData()
         {
-            _NotificationViewModels = new List<NotificationViewModel>();
+            _NotificationViewModels = new ObservableCollection<NotificationViewModel>();
             AddOriginalData();
+        }
+        private void InitCommands()
+        {
+            DeleteNotificationCmd = new DeleteNotificationCommand(this);
         }
         private void AddOriginalData()
         {
             // Load user notifications
             User u1 = new User("micko","micko123","Mica","Lakic");
             User u2 = new User("stefan", "stefan123", "Stefan", "Stefan");
-            
-            _NotificationViewModels.Add(new NotificationViewModel(new Notification(u1, new NotificationElement(), NotificationType.MESSAGE_SENT)));
-            _NotificationViewModels.Add(new NotificationViewModel(new Notification(u1, new NotificationElement(), NotificationType.EVENT_REQUEST)));
-            _NotificationViewModels.Add(new NotificationViewModel(new Notification(u2, new NotificationElement(), NotificationType.EVENT_REQUEST)));
+
+            List<NotificationViewModel> notificationViewModels = new List<NotificationViewModel>();
+            notificationViewModels.Add(new NotificationViewModel(new Notification(1, u1, new NotificationElement(), NotificationType.MESSAGE_SENT)));
+            notificationViewModels.Add(new NotificationViewModel(new Notification(2, u1, new NotificationElement(), NotificationType.EVENT_REQUEST)));
+            notificationViewModels.Add(new NotificationViewModel(new Notification(3, u2, new NotificationElement(), NotificationType.EVENT_REQUEST)));
+            notificationViewModels.ForEach(_NotificationViewModels.Add);
         }
         
-        public List<NotificationViewModel> NotificationViewModels
+        public ObservableCollection<NotificationViewModel> NotificationViewModels
         {
             get => _NotificationViewModels;
             set { _NotificationViewModels = value; RaisePropertyChngedEvent("NotificationViewModels"); }
+        }
+        public ICommand DeleteNotificationCmd
+        {
+            get; private set;
+        }
+        public void DeleteNotification(Notification notification)
+        {
+            foreach (NotificationViewModel viewModel in _NotificationViewModels)
+            {
+                if(viewModel.Notification == notification)
+                {
+                    _NotificationViewModels.Remove(viewModel);
+                    return;
+                }
+            }
         }
     }
 }
