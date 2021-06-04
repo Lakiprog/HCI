0713acd68a0de66baf2aee442752c9c06703aa88
StefanKrnajski <thegamingbook@live.com>
2021-06-04 19:19:39 +0200

    Remove unnecessary code in Homepage

--- EventPlanner/EventPlanner/Pages/Admin/Homepage.xaml.cs
+++ EventPlanner/EventPlanner/Pages/Admin/Homepage.xaml.cs
@@ -20,40 +20,14 @@ namespace EventPlanner.Pages.Admin
     /// </summary>
     public partial class Homepage : Page
     {
-        private Models.Organizer currentO;
-        private Collaborator currentC;
         public Homepage()
         {
             InitializeComponent();
-            
-        }
-
-        private void tutorialButtonHomepageAdmin_Copy_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void ScrollBar_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
-        {
-
         }
 
         private void AddOrganizerButton_Click(object sender, RoutedEventArgs e) {
             var w = new RegisterOrganizerModal();
             w.ShowDialog();
-        }
-
-        private void EditOrganizer_Click(object sender, RoutedEventArgs e)
-        {
-            //currentO = ((Models.Organizer)(sender as Button).DataContext);
-            //var w = new EditOrganizerModal();
-            //w.ShowDialog();
-        }
-
-        private void EditCollaborator_Click(object sender, RoutedEventArgs e)
-        {
-            //var w = new EditCollaboratorModal();
-            //w.ShowDialog();
         }
 
         private void AddCollaboratorButton_Click(object sender, RoutedEventArgs e)
